76b860a47e61102821cc619fc6bf4f7a6722045e
Jatin-1111 <off.jatin1111@gmail.com>
2026-01-28 00:07:51 +0530

    feat: Add API proxy rewrites to Next.js config and create a utility for API URL construction.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://3.110.110.240:5000/api/:path*',
+            },
+        ];
+    },
+};
 
 export default nextConfig;
